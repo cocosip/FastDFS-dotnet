98e1957ff77aaab9c0693a3ae297f9fe98d6993f
virtual <1185513330@qq.com>
2026-04-24 17:16:05 +0800

    refactor(protocol): extend block guard for batch responses

--- src/FastDFS.Client/Protocol/Decoding/ProtocolBlockGuard.cs
+++ src/FastDFS.Client/Protocol/Decoding/ProtocolBlockGuard.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using FastDFS.Client.Exceptions;
 
 namespace FastDFS.Client.Protocol.Decoding
@@ -11,6 +13,30 @@ namespace FastDFS.Client.Protocol.Decoding
                 throw new FastDFSProtocolException(
                     $"{responseName} body length {actualLength} is shorter than the minimum expected {minimumLength} bytes.");
             }
+        }
+
+        public static void EnsureExactBlockMultiple(string responseName, int actualLength, int blockSize)
+        {
+            if (actualLength % blockSize != 0)
+            {
+                throw new FastDFSProtocolException(
+                    $"{responseName} body length {actualLength} is not a multiple of the expected block size {blockSize} bytes.");
+            }
+        }
+
+        public static int ResolveSupportedBlockSize(string responseName, int actualLength, int[] supportedBlockSizes)
+        {
+            if (supportedBlockSizes == null)
+                throw new ArgumentNullException(nameof(supportedBlockSizes));
+
+            foreach (int blockSize in supportedBlockSizes)
+            {
+                if (actualLength % blockSize == 0)
+                    return blockSize;
+            }
+
+            throw new FastDFSProtocolException(
+                $"{responseName} body length {actualLength.ToString(CultureInfo.InvariantCulture)} does not match any supported block size: {string.Join(", ", supportedBlockSizes)}.");
         }
     }
 }
--- tests/FastDFS.Client.Tests/Protocol/Decoding/ProtocolBlockGuardTests.cs
+++ tests/FastDFS.Client.Tests/Protocol/Decoding/ProtocolBlockGuardTests.cs
@@ -15,5 +15,29 @@ namespace FastDFS.Client.Tests.Protocol.Decoding
 
             act.Should().Throw<FastDFSProtocolException>();
         }
+
+        [Fact]
+        public void EnsureExactBlockMultiple_WithMisalignedLength_ShouldThrowFastDFSProtocolException()
+        {
+            Action act = () => ProtocolBlockGuard.EnsureExactBlockMultiple("QueryStoreAllResponse", 41, 40);
+
+            act.Should().Throw<FastDFSProtocolException>();
+        }
+
+        [Fact]
+        public void ResolveSupportedBlockSize_WithSupported592ByteLength_ShouldReturn592()
+        {
+            int blockSize = ProtocolBlockGuard.ResolveSupportedBlockSize("ListStorageServersResponse", 1184, new[] { 600, 592 });
+
+            blockSize.Should().Be(592);
+        }
+
+        [Fact]
+        public void ResolveSupportedBlockSize_WithUnsupportedLength_ShouldThrowFastDFSProtocolException()
+        {
+            Action act = () => ProtocolBlockGuard.ResolveSupportedBlockSize("ListStorageServersResponse", 601, new[] { 600, 592 });
+
+            act.Should().Throw<FastDFSProtocolException>();
+        }
     }
 }
