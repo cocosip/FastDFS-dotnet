8b9eead58d86c7502348304666acb34cb0d3084a
virtual <1185513330@qq.com>
2026-04-27 09:59:23 +0800

    ♻️ refactor(domain): move file id parsing into identifier layer

--- src/FastDFS.Client/Domain/Identifiers/FastDFSFileId.cs
+++ src/FastDFS.Client/Domain/Identifiers/FastDFSFileId.cs
@@ -1,4 +1,4 @@
-using FastDFS.Client.Utilities;
+using System;
 
 namespace FastDFS.Client.Domain.Identifiers
 {
@@ -15,8 +15,90 @@ namespace FastDFS.Client.Domain.Identifiers
 
         public static FastDFSFileId Parse(string fileId, string? defaultGroupName)
         {
-            FileIdHelper.ParseFileId(fileId, out string groupName, out string fileName, defaultGroupName);
+            Split(fileId, defaultGroupName, out string groupName, out string fileName);
             return new FastDFSFileId(GroupName.Create(groupName), fileName);
+        }
+
+        internal static void Split(string fileId, string? defaultGroupName, out string groupName, out string fileName)
+        {
+            if (string.IsNullOrEmpty(fileId))
+                throw new ArgumentException("File ID cannot be null or empty.", nameof(fileId));
+
+            if (LooksLikeStoragePath(fileId))
+            {
+                if (string.IsNullOrEmpty(defaultGroupName))
+                    throw new ArgumentException($"File ID appears to be in simple format (without group name): {fileId}. Please provide a default group name.", nameof(fileId));
+
+                groupName = defaultGroupName!;
+                fileName = fileId;
+            }
+            else
+            {
+                int firstSlashIndex = fileId.IndexOf('/');
+
+                if (firstSlashIndex > 0 && firstSlashIndex < fileId.Length - 1)
+                {
+                    groupName = fileId.Substring(0, firstSlashIndex);
+                    fileName = fileId.Substring(firstSlashIndex + 1);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(defaultGroupName))
+                        throw new ArgumentException($"File ID does not contain group name: {fileId}. Please provide a default group name.", nameof(fileId));
+
+                    groupName = defaultGroupName!;
+                    fileName = fileId;
+                }
+            }
+
+            if (string.IsNullOrEmpty(groupName))
+                throw new ArgumentException("Group name cannot be empty.", nameof(fileId));
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("File name cannot be empty.", nameof(fileId));
+        }
+
+        internal static bool HasExplicitGroupNamePrefix(string fileId)
+        {
+            if (string.IsNullOrEmpty(fileId))
+                return false;
+
+            int firstSlashIndex = fileId.IndexOf('/');
+            return firstSlashIndex > 0 && firstSlashIndex < fileId.Length - 1 && !LooksLikeStoragePath(fileId);
+        }
+
+        private static bool LooksLikeStoragePath(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            int firstSlashIndex = value.IndexOf('/');
+            string firstSegment = firstSlashIndex >= 0 ? value.Substring(0, firstSlashIndex) : value;
+
+            if (firstSegment.Length >= 2
+                && firstSegment[0] == 'M'
+                && char.IsDigit(firstSegment[1])
+                && HasOnlyDigits(firstSegment, 1))
+            {
+                return true;
+            }
+
+            if (firstSegment.Length == 4 && firstSegment.Equals("data", StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            return firstSegment.Length > 4
+                && firstSegment.StartsWith("data", StringComparison.OrdinalIgnoreCase)
+                && HasOnlyDigits(firstSegment, 4);
+        }
+
+        private static bool HasOnlyDigits(string value, int startIndex)
+        {
+            for (int i = startIndex; i < value.Length; i++)
+            {
+                if (!char.IsDigit(value[i]))
+                    return false;
+            }
+
+            return value.Length > startIndex;
         }
     }
 }
--- tests/FastDFS.Client.Tests/Domain/Identifiers/FastDFSFileIdTests.cs
+++ tests/FastDFS.Client.Tests/Domain/Identifiers/FastDFSFileIdTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FastDFS.Client.Domain.Identifiers;
 using FluentAssertions;
 using Xunit;
@@ -21,6 +22,32 @@ namespace FastDFS.Client.Tests.Domain.Identifiers
             var fileId = FastDFSFileId.Parse("M00/00/00/file.txt", "group1");
 
             fileId.GroupName.Value.Should().Be("group1");
+            fileId.FileName.Should().Be("M00/00/00/file.txt");
+        }
+
+        [Fact]
+        public void Parse_WithStoragePathAndNoDefaultGroup_ShouldThrowArgumentException()
+        {
+            Action act = () => FastDFSFileId.Parse("M00/00/00/file.txt", null);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Parse_WithAmbiguousGroupPrefix_ShouldPreserveGroup()
+        {
+            var fileId = FastDFSFileId.Parse("data-prod/M00/00/00/file.txt", null);
+
+            fileId.GroupName.Value.Should().Be("data-prod");
+            fileId.FileName.Should().Be("M00/00/00/file.txt");
+        }
+
+        [Fact]
+        public void Parse_WithM1PrefixedGroup_ShouldPreserveGroup()
+        {
+            var fileId = FastDFSFileId.Parse("M1group/M00/00/00/file.txt", null);
+
+            fileId.GroupName.Value.Should().Be("M1group");
             fileId.FileName.Should().Be("M00/00/00/file.txt");
         }
     }
